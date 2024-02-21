30eeaba6a5ebb1ccc3710ddfb94cb4b4aafd84c4
rifat <rifatscc1747@gmail.com>
2024-02-21 19:02:55 +0600

    project files updated

--- travel/urls.py
+++ travel/urls.py
@@ -1,5 +1,33 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for Travel project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('', include('home.urls'))
 ]
+
+
+# static helper function used to serve media files
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+# url of media files
+# http://127.0.0.1:8000/media-file/pics/Indonesia.jpg 
