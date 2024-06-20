2bd8363b0671fdf13c6af90364ec0bff99c28533
rifat <rifatscc1747@gmail.com>
2024-06-21 03:02:02 +0600

    internal project title updated

--- home/views.py
+++ home/views.py
@@ -4,4 +4,5 @@ from .models import Destination
 # Create your views here.
 def index(request):
     dests = Destination.objects.all()
+    # print(dests)
     return render(request, 'index.html', {'destinations': dests})
